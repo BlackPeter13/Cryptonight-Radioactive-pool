a825571b76c4176b5a3e61011768a1c6d4b2b1a0
BlackPeter13 <60829180+BlackPeter13@users.noreply.github.com>
2022-12-06 20:44:21 +0100

    Add files via upload

---
+++ website_example/config.js
@@ -0,0 +1,12 @@
+var parentCoin = "INF8";
+var api = "http://radioactive.sytes.net:8117";
+
+var email = "support@poolhost.com";
+var telegram = "https://t.me/YourPool";
+var discord = "https://discordapp.com/invite/YourPool";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var themeCss = "themes/default.css";
+var defaultLang = "en";
+
